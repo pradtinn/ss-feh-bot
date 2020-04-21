053e7a23b3d1319fa165988427e8746d45ab49fb
pradtinn <prad.tinn@gmail.com>
2020-04-21 14:27:14 -0400

    check for space issue

--- src/web_scraper.js
+++ src/web_scraper.js
@@ -117,7 +117,7 @@ module.exports = {
                 tableCol.each( function(colCount=0) {
                     if (colCount == 1) {
                         var skillName = rowData(this).text().toLowerCase();
-                        console.log(skillName, name, name.localeCompare(skillName));
+                        console.log('\''+skillName+'\'', '\''+name+'\'', name.localeCompare(skillName));
                         if (name.localeCompare(skillName) == 0) {
                             console.log('found match!');
                             found = true;
