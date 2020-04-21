cd6632b49002b4cc182fdb044a1f4a1cf1868d0e
pradtinn <prad.tinn@gmail.com>
2020-04-20 23:22:54 -0400

    none

--- src/bot.js
+++ src/bot.js
@@ -89,7 +89,6 @@ function handleError(msg) {
 bot.on('message', msg => {
     var message = msg.content;
     var channel = msg.channel;
-    console.log(channel.id,":",message);
     if (message.substring(0, 1) == '!') {
         if (message.substring(1) == 'heelp') {
             msg.member.createDM().then((dmchannel) => {
