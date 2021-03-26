f178ffed659b67df0b4c37acbe1230f32bc30810
pradtinn <prad.tinn@gmail.com>
2021-03-26 18:25:54 -0400

    e

--- get_skill.py
+++ get_skill.py
@@ -75,11 +75,10 @@ if passive:
 else:
     out['desc'] = skill_row.find_all('td')[2].get_text()
 
-inherit_restr = skill_table.find_all('tr')[-1].td
+inherit_restr = skill_table.find_all('tr')[level-1].find_all('td')[-1]
 out['inherit'] = ''
 for restr in inherit_restr.contents:
     if restr.name == 'a':
-        print(restr['title'])
         title = restr['title'].replace('bow', 'Bow').replace('Staff', 'Colorless Staff')
         out['inherit'] += title
     else:
